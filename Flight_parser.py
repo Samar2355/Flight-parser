2440f4a92691bbd25cc19b1a6a03d067a75803ed
Samar Joshi <167465181+Samar2355@users.noreply.github.com>
2025-11-22 17:08:43 +0000

    added error validation, file i/o

---
+++ Flight_parser.py
@@ -0,0 +1,34 @@
+# Name: Samar Joshi
+# Student ID: 241ADB043
+# Github:
+#importing libraries
+import csv
+import json
+#opening csv file and reading it line by line
+with open('db.csv',encoding='utf-8-sig') as opener:
+    reader = csv.reader(opener,delimiter=',')
+    # skipping header file
+    header = next(reader)
+    for line in reader:
+        if line[0].startswith('#'): # skipping comment lines
+         continue
+        flight = dict(zip(header, line))  # combining header + row
+        # for easier changing
+        flight_id = flight['flight_id']
+        origin = flight['origin']
+        destination = flight['destination']
+        departure = flight['departure_datetime']
+        arrival = flight['arrival_datetime']
+        price = flight['price']
+        # for checking the validitiy of flight details
+        if not (2 <=len(flight_id) <= 8):
+            print("Invalid Flight ID")
+        if not len(origin) == 3 or not origin.isupper():
+            print("Invalid Origin")  
+        if not len(destination) == 3 or not destination.isupper():
+            print("Invalid Destination")
+        if not price > 0:
+            print("Invalid Price")
+        if not 
+            
+        print(flight)
